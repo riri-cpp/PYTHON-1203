1171c7971d149ba00a68f3278ee97d0fc68626e9
riri-cpp <pasiarheman7@gmail.com>
2025-01-31 10:46:47 +0800

    Add files via upload

---
+++ escape_sequences.py
@@ -0,0 +1,6 @@
+for i in range(1,6):
+    num = 7 * i
+    if i % 2 == 0:
+        print(f"\t{num}")
+    else:
+        print(num)
---
+++ temp_convert.py
@@ -0,0 +1,3 @@
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9)/5 + 32
+print(f"{celsius:.2f} Celsius is {fahrenheit:.2f} Fahrenheit.")
